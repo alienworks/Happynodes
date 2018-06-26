f66f57e4d747e53c56e9c03f84e38fe478f2267b
i25959341 <karlson.lee09@gmail.com>
2018-06-26 15:27:42 +0100

    refactor

--- neo-collector/delete/deleteOldTx.py
+++ neo-collector/delete/deleteOldTx.py
@@ -1,5 +1,5 @@
 import psycopg2
-from config import CONNECTION_STR, DSN
+from config import CONNECTION_STR
 import time
 
 connect_str = CONNECTION_STR
@@ -8,6 +8,8 @@ while True:
     conn = psycopg2.connect(connect_str)
 
     cursor = conn.cursor()
+
+    print("delete tx")
 
     cursor.execute("delete \
     FROM unconfirmed_tx \
@@ -18,5 +20,7 @@ while True:
     conn.commit()
     cursor.close()
     conn.close()
+
+    print("Sleeping")
     # Run hourly
     time.sleep(60*60) 
