ae86da3e2b7bb8f2261cfa2bb98378016b077400
wy <wy@users.noreply.github.com>
2018-08-12 15:32:19 +0100

    UI Improvements - Added Menu and navigation

--- neo-interface/src/App.js
+++ neo-interface/src/App.js
@@ -11,10 +11,12 @@ import NodeInfo from './components/NodeInfo'
 import NodeEdges from './components/NodeEdges'
 import NetworkGraph from './components/NetworkGraph'
 import NetworkTable from './components/NetworkTable'
+import HistoryTest from './components/History'
 import logo from './HappyNodes_Logo.png'
 import f27logo from './27-light-red.png'
 import UnconfirmedTx from './components/UnconfirmedTx'
-import createHistory from 'history/createBrowserHistory'
+import UnconfirmedTxInfo from './components/UnconfirmedTxInfo'
+import MainMenu from './components/menu'
 import ReactGA from 'react-ga';
 
 ReactGA.initialize(process.env.REACT_APP_GA_KEY);
@@ -25,7 +27,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      
+      <div className="App" id="outer-container">
+      <MainMenu/>
+        <main id="page-wrap">
         <header className="App-header">
         <Link to='/'><img src={logo} className="App-logo" alt="logo" /></Link>
           
@@ -33,20 +38,23 @@ class App extends Component {
             <LastBlock/>
             <BlockTime/>
         </header>
-
+        
         <div className="App-intro">
           <div className="container-fluid">
           <Route exact path="/" render={({match})=><NodesByRegion/>} />
           <Route path="/:id(\d+)" render={({match})=><NodesByRegion node_id={match.params.id}/>} />
-          <div className="col col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <Route exact path="/unconfirmed" render={({match})=><UnconfirmedTx/>} />
+          <Route path="/unconfirmedtxinfo/:tx/:connection_id" render={({match})=><UnconfirmedTx/>} />
+          <div className="col col-lg-9 col-md-9 col-sm-12 col-xs-12">
             <Route path="/:id(\d+)" render={({match})=><NodeInfo node_id={match.params.id}/>} />
             <Route path="/:id(\d+)" render={({match})=><NodeEdges node_id={match.params.id}/>} />
+            <Route path="/unconfirmedtxinfo/:connection_id/:tx" render={({match})=><UnconfirmedTxInfo  connection_id={match.params.connection_id} tx={match.params.tx}/>} />
             <Route exact path="/" render={({match})=><NetworkGraph/>} />
             
           </div>
           <Route exact path="/table" render={({match})=><NetworkTable/>} />
-          <Route exact path="/" render={()=><UnconfirmedTx/>} />
-          <Route path="/:id(\d+)" render={()=><UnconfirmedTx/>} />
+          <Route exact path="/history" render={({match})=><HistoryTest/>} />
+          
           </div>
         </div>
         <footer className="App-footer">
@@ -56,7 +64,7 @@ class App extends Component {
         </div>
         <a href="https://github.com/F27Ventures" rel="noopener noreferrer" target="_blank"><img src={f27logo} className="Footer-app-logo" alt="logo" /></a>
             </footer>
-      </div>
+      </main></div>
     );
   }
 }
---
+++ neo-interface/src/components/History.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { connect } from 'react-refetch'
+import config from './config'
+import  {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+import ReactResizeDetector from 'react-resize-detector';
+
+class HistoryTest extends Component {
+
+
+    render() {
+
+        const { historytest } = this.props;
+
+
+        if (historytest.pending) {
+            return (
+                <div className="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div className="jumbotron">
+                        <h2>Loading...</h2>
+                    </div></div>
+            )
+        } else if (historytest.rejected) {
+
+            return (
+                <div className="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div>
+                        error
+                </div></div>
+            )
+        } else if (historytest.fulfilled) {
+
+          
+            
+          let rows = historytest.value['data'];
+            
+          const SpecialAreaChart = ({ width, height }) => (
+
+            <AreaChart width={width} height={400} data={rows}
+            margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="date"/>
+        <YAxis/>
+        <Tooltip/>
+        <Area type='monotone' dataKey='total' stroke='#8884d8' fill='#8884d8' />
+        <Area type='monotone' dataKey='totalonline' stroke='#82ca9d' fill='#82ca9d' />
+      </AreaChart>  
+            
+          );
+            
+
+            return (
+                <div className="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div className="jumbotron nodes">
+
+                        <h2>Historic View: Online and Total Nodes Daily</h2>
+                        <hr style={{
+                        'borderColor': '#78cadd', 'borderWidth': '3px'
+                    }} />      
+
+                    <h3>We released V1.1.0 (Node Discovery) at the start of August which is why the number of nodes we know about increased significantly after that.</h3>             
+
+                       
+                       <ReactResizeDetector handleWidth handleHeight>
+                    <SpecialAreaChart />
+                    </ReactResizeDetector>
+
+
+                    </div></div>
+
+            )
+        }
+    }
+}
+
+
+
+export default connect((props) => ({
+    historytest: {
+        url: config.api_url.concat(`/historic/network/size/daily`)
+    }
+}))(HistoryTest)
