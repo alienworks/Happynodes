802605abd67b4b6e597f043f06fdfbfedfc338fe
i25959341 <karlson.lee09@gmail.com>
2018-08-16 22:48:22 +0100

    test fail

--- neo-interface/src/components/ClientSideLatency.test.js
+++ neo-interface/src/components/ClientSideLatency.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import ClientSideLatency from './ClientSideLatency';
 import renderer from 'react-test-renderer';
 
-Date.now = jest.fn(() => 1487076708000)
+// Date.now = jest.fn(() => 1487076708000)
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
