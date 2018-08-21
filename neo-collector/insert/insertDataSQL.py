79fc193e2585b93fe01e786dac6b39b8e9d5f215
i25959341 <karlson.lee09@gmail.com>
2018-08-21 22:02:42 +0100

    print result

--- neo-collector/insert/insertDataSQL.py
+++ neo-collector/insert/insertDataSQL.py
@@ -152,11 +152,11 @@ async def main():
     print("size of endpoints list ", len(results))
 
     t0 = time.time()
-    result = await asyncio.wait([update(url, id) for id, url in results ])
+    data = await asyncio.wait([update(url, id) for id, url in results ])
 
     t1 = time.time()
     print('Took %.2f ms' % (1000*(t1-t0)))
-    print(results)
+    print(data)
 
 
 loop = asyncio.get_event_loop()
