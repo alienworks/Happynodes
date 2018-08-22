dcb2cabf0a1d43f8e2d8c1d5ff77312ff851ee25
i25959341 <karlson.lee09@gmail.com>
2018-08-22 14:08:17 +0100

    test

--- neo-collector/insert/insertDataSQL.py
+++ neo-collector/insert/insertDataSQL.py
@@ -15,7 +15,7 @@ import logging
 ssl.match_hostname = lambda cert, hostname: True
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
