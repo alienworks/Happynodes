d1a652fa5f3a4cafaa7381dee95b20868e89f9bf
i25959341 <karlson.lee09@gmail.com>
2018-11-03 21:29:29 +0000

    fix summary

--- neo-collector/insert/insertDataSQL.py
+++ neo-collector/insert/insertDataSQL.py
@@ -291,8 +291,8 @@ def insertRedisBlockheight(blockheightData):
             logger.info("blockheight {}".format(blockcount))
             r.hset(redisNamespace + 'node', connectionId, json.dumps(node_info))
 
-    r.set(redisNamespace+'lastblock', max_blockheight_ts)
-    r.set(redisNamespace+'blocktime', max_blockheight)
+    # r.set(redisNamespace+'lastblock', max_blockheight_ts)
+    r.set(redisNamespace+'bestblock', max_blockheight)
     t1 = time.time()
     logger.info('Redis Took %.2f ms' % (1000*(t1-t0)))
 
