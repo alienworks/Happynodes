d5c3cb84bddb2a3fd2cbba1bd031ad55cdae655d
i25959341 <karlson.lee09@gmail.com>
2018-12-15 11:31:59 +0000

    debug

--- neo-collector/insert/insertDataSQL.py
+++ neo-collector/insert/insertDataSQL.py
@@ -312,8 +312,8 @@ def batchInsert(cursor, sqlScript, datalist):
 
 def insertRedisBlockInfo(max_block_result_data):
     r = get_redis_instance()
-    print("max_block_result_data ", max_block_result_data)
     ts, max_block_result = max_block_result_data[0]
+    logger.info("hello mother fuckers", max_block_result)
     r.set(redisNamespace+'lastestblocksize', max_block_result['size'])
     r.set(redisNamespace+'lastesttxcount', len(max_block_result['tx']))
     r.set(redisNamespace+'lastestblocktime', len(max_block_result['time']))
